7932751e832fe69737764c784709e50c41f05131
dubrovsky <33762730+kufuna@users.noreply.github.com>
2019-04-11 18:43:11 +0400

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+
+// var arr = [1,2,3,4,5,8];
+
+// for (var i = 0; i < arr.length; i++) {
+// 	var element = document.createElement('div');
+// 	element.classList.add('box');
+// 	element.innerHTML = arr[i];
+// 	document.querySelector('#game').append(element);
+// 	var box = document.querySelectorAll('.box');
+// }
+
+// var level = [[2,10]];
+
+// var checkNumber = function(currentLevel){
+// 	if (parseInt(this.innerHTML) + level[0][0] === level[0][1] )  {
+// 		alert('you win')
+// 	}
+// }
+
+// for (var i = 0; i < box.length; i++) {
+// 	box[i].addEventListener('click',checkNumber)
+// }
+
+var level , numbers , time ; 
+
+}
